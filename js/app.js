4b28625b764a84b37c5254604b2cd949eb6ccf9e
chenyuheng <pass@chenyuheng.cn>
2023-03-22 18:42:43 -0400

    basic functions

--- js/app.js
+++ js/app.js
@@ -24,14 +24,14 @@ function ClockOptionRadios() {
 }
 
 function handleClockOptionChange(event) {
-    console.log(event.target.value);
+    clockOption = event.target.value;
 }
 
 class Time extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: new Date(),
+            dateStr: this.getOffsetTime("s"),
         }
     }
 
@@ -47,38 +47,54 @@ class Time extends React.Component {
     }
 
     tick() {
+        if (clockOption === "s" || clockOption === "m") {
+            this.setState({
+                dateStr: this.getOffsetTime()
+            });
+            return;    
+        }
         this.setState({
-            date: new Date()
+            dateStr: new Date().toLocaleTimeString(window.navigator.language, { timezone: this.props.timezone })
         });
     }
 
     render() {
         return (
-            <span>{this.state.date.toLocaleTimeString()}</span>
+            <span className="clock">{this.state.dateStr}</span>
         );
+    }
+
+    getOffsetTime() {
+        let date = new Date();      
+        let minuteOffset = this.props.longitude / 15 * 60;
+        if (clockOption === "m") {
+            minuteOffset = Math.floor(minuteOffset);
+        }
+        date = new Date(new Date().getTime() + minuteOffset * 60 * 1000);
+        return date.toLocaleTimeString(window.navigator.language, { timezone: "UTC" });
     }
 }
 
 class CityClock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name: props.name,
-            timezone: props.timezone,
-            longitude: props.longitude
-        };
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onChange(this.props.name);
     }
 
     render() {
         return (
             <li className="collection-item avatar">
-            <i className="material-icons circle green">insert_chart</i>
+            <i className="material-icons circle green">access_time</i>
             <p>
-            <span className="brand-logo">{this.state.name}<Time /> </span><br />
-                Longitute: {this.state.longitude} | Time Zone: {this.state.timezone}
+            <Time longitude={this.props.longitude} timezone={this.props.timezone} /> | {this.props.name}<br />
+                Longitute: {longitudeToString(this.props.longitude)} | Time Zone: {this.props.timezone}
             </p>
             <p className="secondary-content">
-            <a className="waves-effect waves-light btn-small red"><i className="material-icons">delete</i></a>
+            <a className="waves-effect waves-light btn-small red" onClick={this.handleDelete}><i className="material-icons">delete</i></a>
             </p>
         </li>
         );
@@ -91,20 +107,30 @@ class AddCityInput extends React.Component {
         this.state = {
             name: "",
         };
+        this.handleAdd = this.handleAdd.bind(this);
     }
 
     componentDidMount() {
         var elems = document.querySelectorAll('.autocomplete');
+        let emptyCities = {};
+        for (let city in cities) {
+            emptyCities[city] = null;
+        }
         var instances = M.Autocomplete.init(elems, {
-            data: {
-                "北京": null,
-                "上海": null,
-                "广州": null,
-                "New York": null,
-                "Atlanta": null,
-                "San Francisco": null,
-            }
+            data: emptyCities
         });
+    }
+
+    handleAdd() {
+        let cityName = document.getElementById("add-city-input").value;
+        if (cityName === "") {
+            return;
+        }
+        if (cities[cityName] === undefined) {
+            M.toast({ html: "City not found" });
+            return;
+        }
+        this.props.onChange(cityName);
     }
 
     render() {
@@ -112,11 +138,11 @@ class AddCityInput extends React.Component {
             <div className="col s12 m6">
                 <div className="input-field col s8">
                     <i className="material-icons prefix">location_city</i>
-                    <input type="text" id="autocomplete-input" className="autocomplete" />
-                    <label htmlFor="autocomplete-input">Add a City</label>
+                    <input type="text" id="add-city-input" className="autocomplete" />
+                    <label htmlFor="add-city-input">Add a City</label>
                 </div>
                 <div className="input-field col s4">
-                    <div className="btn"><i className="material-icons">add</i></div>
+                    <div className="btn" onClick={this.handleAdd}><i className="material-icons">add</i></div>
                 </div>
             </div>
         );
@@ -129,31 +155,105 @@ function Title() {
     );
 }
 
-function App() {
-    return (
-        <div className="container">
-            <div className="row">
-                <Title />
-            </div>
-            <div className="row">
-                <div className="col s12 m6">
-                    <ClockOptionRadios />
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cities: ["北京", "喀什", "牡丹江", "New York", "Atlanta", "San Francisco"]
+        };
+        this.handleAddCity = this.handleAddCity.bind(this);
+        this.handleDeleteCity = this.handleDeleteCity.bind(this);
+    }
+
+    handleAddCity(cityName) {
+        if (this.state.cities.includes(cityName)) {
+            M.toast({ html: "City already added" });
+            return;
+        }
+        this.setState((state, props)=>({
+            cities: state.cities.concat([cityName])
+        }));
+        M.toast({ html: "City added" });
+    }
+
+    handleDeleteCity(cityName) {
+        this.setState((state, props)=>({
+            cities: state.cities.filter((city) => city !== cityName)
+        }));
+        M.toast({ html: "City deleted" });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Title />
                 </div>
-                <AddCityInput />
+                <div className="row">
+                    <div className="col s12 m6">
+                        <ClockOptionRadios />
+                    </div>
+                    <AddCityInput onChange={this.handleAddCity} />
+                </div>
+                <div className="row">
+                    <ClockList cities={this.state.cities} onChange={this.handleDeleteCity} />
+                </div>
             </div>
-            <div className="row">
-                <ul className="collection">
-                    <CityClock name="北京" timezone="UTC+8" longitude="116.407395" />
-                </ul>
-            </div>
-        </div>
-    );
+        );
+    }
+}
+
+class ClockList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete(cityName) {
+        this.props.onChange(cityName);
+    }
+
+    render() {
+        const shownCities = this.props.cities;
+        const cityList = shownCities.map((city) =>
+            <CityClock key={city} name={city} longitude={cities[city].longitude} timezone={cities[city].timezone} onChange={this.handleDelete} />
+        );
+        return (
+            <ul className="collection">
+                {cityList}
+            </ul>
+        );
+    }
+}
+
+let clockOption = "s";
+let cities = {};
+
+function longitudeToString(longitude) {
+    let str = "";
+    if (longitude > 0) {
+        str += "E";
+    } else {
+        str += "W";
+    }
+    let absLongitude = Math.abs(longitude);
+    let degree = Math.floor(absLongitude);
+    let minute = Math.floor((absLongitude - degree) * 60);
+    str += degree + "°" + minute + "'";
+    return str;
 }
 
 function init() {
-    const root = ReactDOM.createRoot(
-        document.getElementById("root")
-    );
-    root.render(<App />);
-
+    fetch("data/cities.json")
+        .then(response => response.json())
+        .then(data => {
+            for (let city of data) {
+                cities[city.name] = city;
+            }
+        }).then(() => {
+            const root = ReactDOM.createRoot(
+                document.getElementById("root")
+            );
+            root.render(<App />);
+        });
 }
